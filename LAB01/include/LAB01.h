4a94348be1f055a8fef6234cdd69ff173fec53c2
Jaroslaw Roszyk <roszyk.jarek@gmail.com>
2022-03-12 21:02:08 +0100

    xd

--- LAB01/include/LAB01.h
+++ LAB01/include/LAB01.h
@@ -1,41 +1,8 @@
 #pragma once
 #include <string>
 #include <iostream>
-//    auto separatOddFromEven(std::vector<int> arr) -> void
-//    {
-//        auto isEven = [](int a){return a % 2 == 0;};
-//        auto isGreater = [](int a,int b){return a > b;};
-//        std::sort(arr.begin(),arr.end());
-//        for (int i = 0; i < arr.size(); i++)
-//        {
-//            if(isEven(arr[0]))
-//            {
-//            }
-//            if (arr[i] % 2 == 0)
-//                std::cout << "Even: " << arr[i] << " ";
-//        }
+#include <vector>
 
-//    auto separatOddFromEven(int arr[], int n) -> void
-//    {
-//        int i = 1;
-//        int j = n-1;
-//        auto isGreater = [](int a,int b){return a > b;};
-//        if (j%2!=0)
-//            j--;
-//
-//        // swapping till half of array
-//        while (i<j)
-//        {
-//            swap(arr[i], arr[j]);
-//            i += 2;
-//            j -= 2;
-//        }
-//        // Sort first half in increasing
-//        std::sort(arr, arr+(n+1)/2);
-//
-//        // Sort second half in decreasing
-//        sort(arr+(n+1)/2, arr+n, std::greater<int>());
-//    }
 struct Separate
 {
     template<typename T>
